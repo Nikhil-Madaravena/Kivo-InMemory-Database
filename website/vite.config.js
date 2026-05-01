fb2fb62eed89df23f03eb8f626b86810c15ba86f
Nikhil Madaravena <nikhil.madaravena@gmail.com>
2026-05-02 00:59:16 +0530

    feat: project website and docs

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kivo-InMemory-Database/',
+  base: '/kivo-website/',
   build: {
     outDir: '../docs',
     emptyOutDir: true
